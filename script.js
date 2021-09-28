138d292960d0ef6af63a8eb4d36c5547127762ec
Nick <nickmagi24@gmail.com>
2021-09-28 12:47:01 -0400

    Psuedo

---
+++ script.js
@@ -0,0 +1,32 @@
+//const gameBoard = (() => {})();
+    //store board as empty array
+    //generate via loop? i < 9,etc (alternatively create in html)
+
+
+
+
+
+
+
+
+// displayController
+    //lets players make marks on baored
+    //takes "taken spots" into account
+    
+    //player 1
+
+    //player 2
+
+
+
+
+//Checks for when game is over
+    //determine winner of looser
+    //check for tie
+    // let winConditions = wins array 
+
+
+
+
+
+//redner elements on page
