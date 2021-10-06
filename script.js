92abf29a3d68b2bf0103847bd472feb633c5b0aa
Nick <nickmagi24@gmail.com>
2021-10-06 16:45:45 -0400

    Gameboard array and functions

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-//const gameBoard = (() => {})();
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", "", ]
+
+    const setBoard = (index, sign) => {
+        if (index > board.length) return;
+        board[index] = sign;
+    }
+
+    const getBoard = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    }
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+            
+        }
+    }
+    return {setBoard, getBoard, reset};
+})();
     //store board as empty array
+
     //generate via loop? i < 9,etc (alternatively create in html)
+    //const setField 
+
+
+    //const getField
 
 
 
+    //const reset =
 
 
 
-
+    //return {} //Factory way
 
 // displayController
     //lets players make marks on baored
