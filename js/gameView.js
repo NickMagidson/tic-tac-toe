859365894253076c253de32886ab32fa984b7f13
Nick <nickmagi24@gmail.com>
2021-10-19 17:05:40 -0400

    Game view and innerHTML

---
+++ js/gameView.js
@@ -0,0 +1,18 @@
+export default class GameView {
+    constructor(root) {
+        this.root = root;
+        this.root.innerHTML = `
+            <div class="header">
+                <div class="header-turn">
+                    X's turn
+                </div>
+                <div class="header-status">
+                    In Progress
+                </div>
+                <button type="button" class="header-restart">
+                    <i class="material-icons>refresh</i>
+                </button>
+            </div>
+        `;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 import Game from "./game.js";
+import GameView from "./gameView.js";
 
 let game = new Game();
-
-game.makeMove(0) // x
-game.makeMove(3) // o
-game.makeMove(7) // x
-game.makeMove(4) // o
-game.makeMove(2) // x
-game.makeMove(5) // x
-console.log(game.board);
-console.log(game.findWinConditions());
+let gameView = new GameView(document.getElementById('app'));
