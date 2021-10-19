39da4e3d90c0023e1022bb3cd218b880f48d99d7
Nick <nickmagi24@gmail.com>
2021-10-19 17:53:47 -0400

    Event listeners

--- js/gameView.js
+++ js/gameView.js
@@ -3,18 +3,59 @@ export default class GameView {
         this.root = root;
         this.root.innerHTML = `
             <div class="header">
-                <div class="header-turn">
-                    X's turn
-                </div>
-                <div class="header-status">
-                    In Progress
-                </div>
-                <button type="button" class="header-restart"><span class="material-icons">
-                refresh
-                </span>
-                    <i class="material-icons>refresh</i>
-                </button>
-            </div>
-        `;
+            <div class="header-turn"></div>
+            <div class="header-status"> In Progress</div>
+            <button type="button" class="header-restart"> 
+                <i class="material-icons">refresh</i>
+            </button>
+        </div>
+        <div class="board">
+            <div class="board-tile" data-index="0"></div>
+            <div class="board-tile" data-index="1"></div>
+            <div class="board-tile" data-index="2"></div>
+            <div class="board-tile" data-index="3"></div>
+            <div class="board-tile" data-index="4"></div>
+            <div class="board-tile" data-index="5"></div>
+            <div class="board-tile" data-index="6"></div>
+            <div class="board-tile" data-index="7"></div>
+            <div class="board-tile" data-index="8"></div>
+        </div>
+        `;  
+
+        this.onTileClick = undefined;
+        this.onRestartClick = undefined;
+
+        this.root.querySelectorAll(".board-tile").forEach(tile => {
+            tile.addEventListener('click', () => {
+                if (this.onTileClick) {
+                    this.onTileClick(tile.dataset.index);  
+                }
+            })
+        })
+
+
+        this.root.querySelector('.header-restart').addEventListener('click' , () => {
+            if (this.onRestartClick) {
+                this.onRestartClick();
+            }
+        })
+    }
+
+    update(game) {
+        this.updateTurn(game);
+        this.updateStatus(game);
+        this.updateBoard(game);
+    }
+
+    updateTurn(game) {
+        
+    }
+
+    updateStatus(game) {
+
+    }
+
+    updateBoard(game) {
+        
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ import GameView from "./gameView.js";
 
 let game = new Game();
 let gameView = new GameView(document.getElementById('app'));
+
+// Functions
+gameView.onTileClick = function(i) {
+    console.log(`Tile clicked: ${i}`)
+}
+
+gameView.onRestartClick = function() {
+    console.log("Game restarted")
+}
